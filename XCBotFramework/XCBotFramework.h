60c09344e1b98a124083a4cb93775ec65eb3c68a
BJ Homer <bjhomer@gmail.com>
2014-07-22 00:06:29 -0600

    Initial commit.

---
+++ XCBotFramework/XCBotFramework.h
@@ -0,0 +1,19 @@
+//
+//  XCBotFramework.h
+//  XCBotFramework
+//
+//  Created by BJ Homer on 7/22/14.
+//  Copyright (c) 2014 BJ Homer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XCBotFramework.
+FOUNDATION_EXPORT double XCBotFrameworkVersionNumber;
+
+//! Project version string for XCBotFramework.
+FOUNDATION_EXPORT const unsigned char XCBotFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCBotFramework/PublicHeader.h>
+
+
